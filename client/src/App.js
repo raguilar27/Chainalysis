47cc329fef62f4e336d1fef715542f392d172b54
Rogelio Aguilar <raguilar27@github.com>
2021-10-17 00:01:25 -0700

    gets coinbase btc price

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
 import "./App.css";
+import coinbaseAPI from "./api/coinbaseAPI";
+//import krakenAPI from "./api/krakenAPI";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [coinbase, setCoinbase] = useState([]);
+
+  useEffect(() => {
+    coinbaseAPI.getBuyBtc().then((price) => {
+      console.log(price);
+      setCoinbase(coinbase.concat({ BuyBtc: price }));
+    });
+  }, []);
+
   return (
     <div className="App">
       <h4>Crypto Prices</h4>
+      <h6>Coinbase:</h6>
+      <p>Bitcoin buy price: ${coinbase.BuyBtc}</p>
     </div>
   );
 }
