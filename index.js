6c927a2a10e46021569b1e5350f2263b6bddea21
Rogelio Aguilar <raguilar27@github.com>
2021-10-16 23:55:41 -0700

    set up headers for frontend api calls

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
+// Backend Server File
 const express = require("express");
-const coinbase = require("./modules/coinbase");
-const kraken = require("./modules/kraken");
+const coinbase = require("./routes/coinbase");
+const kraken = require("./routes/kraken");
 require("dotenv").config();
 const app = express();
+
+// CORS HEADERS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // if it's not set to all domains, update '*' to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
 
 // Route to kraken api
 app.use("/kraken", kraken);
 // Route to coinbase api
 app.use("/coinbase", coinbase);
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const port = process.env.PORT || 3001;
+app.listen(port, () => console.log(`Server running on port ${port}`));
