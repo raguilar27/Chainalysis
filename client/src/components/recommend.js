074b2cda088082e67ec9f7700b4127d7c9bc86ff
Rogelio Aguilar <raguilar27@github.com>
2021-10-20 04:08:45 -0700

    completed recommedation component

--- client/src/components/recommend.js
+++ client/src/components/recommend.js
@@ -1,27 +1,66 @@
+import BitcoinLogo from "../logos/bitcoin_logo.svg.png";
+import EthereumLogo from "../logos/ethereum.png";
+import coinbaseLogo from "../logos/Coinbase.svg.png";
+import krakenLogo from "../logos/krakenlogo.png";
+
 const Recommend = ({ buyBtc, buyEth, sellEth, sellBtc }) => {
   return (
     <div className="recommend">
       <h2>Recommended Exchange</h2>
-      {buyBtc.includes("kraken") ? (
-        <p>Best Bitcoin buy price: Kraken</p>
-      ) : (
-        <p>Best Bitcoin buy price: Coinbase</p>
-      )}
-      {sellBtc.includes("kraken") ? (
-        <p>Best Bitcoin sell price: Kraken</p>
-      ) : (
-        <p>Best Bitcoin sell price: Coinbase</p>
-      )}
-      {buyEth.includes("kraken") ? (
-        <p>Best Ethereum buy price: Kraken</p>
-      ) : (
-        <p>Best Ethereum buy price: Coinbase</p>
-      )}
-      {sellEth.includes("kraken") ? (
-        <p>Best Ethereum sell price: Kraken</p>
-      ) : (
-        <p>Best Ethereum sell price: Coinbase</p>
-      )}
+      <div className="recommendation">
+        <div className="recommend-crypto">
+          <img src={BitcoinLogo} alt="Crypto logo" />
+          <div className="recommend-exchange">
+            <div className="new">
+              {buyBtc.includes("kraken") ? (
+                <p>
+                  Best Buy Price: <img src={krakenLogo} alt="Kraken Logo" />
+                </p>
+              ) : (
+                <p>
+                  Best Buy Price: <img src={coinbaseLogo} alt="Coinbase Logo" />
+                </p>
+              )}
+              {sellBtc.includes("kraken") ? (
+                <p>
+                  Best Sell Price: <img src={krakenLogo} alt="Kraken Logo" />
+                </p>
+              ) : (
+                <p>
+                  Best Sell Price:{" "}
+                  <img src={coinbaseLogo} alt="Coinbase Logo" />
+                </p>
+              )}
+            </div>
+          </div>
+        </div>
+        <div className="recommend-crypto">
+          <img src={EthereumLogo} alt="Crypto logo" />
+          <div className="recommend-exchange">
+            <div className="new">
+              {buyEth.includes("kraken") ? (
+                <p>
+                  Best Buy Price: <img src={krakenLogo} alt="Kraken Logo" />
+                </p>
+              ) : (
+                <p>
+                  Best Buy Price: <img src={coinbaseLogo} alt="Coinbase Logo" />
+                </p>
+              )}
+              {sellEth.includes("kraken") ? (
+                <p>
+                  Best Sell Price: <img src={krakenLogo} alt="Kraken Logo" />
+                </p>
+              ) : (
+                <p>
+                  Best Sell Price:{" "}
+                  <img src={coinbaseLogo} alt="Coinbase Logo" />
+                </p>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
